1d7959ec5cd6f00e9bbd6357530a9f290be7f70d
MuriloLim4SS <murilolimassilva24@gmail.com>
2024-02-01 19:11:42 -0300

    Programa de criação de Annagramas -Version 2.0

--- __anagram__.py
+++ __anagram__.py
@@ -1,15 +1,23 @@
-from random import shuffle
-import random as r
-
+import random
 
 class Anagram:
     def __init__(self, word):
-        self.word = list(word)
+        self.word = word
+        self.word_list = None
 
     def anagram(self):
-        r.shuffle(self.word)
-        return ''.join(self.word)
+        self.word_list = []
+        for i in self.word.split():
+            self.word_list.append(i)
 
+        shuffled_words = []
+        for item in self.word_list:
+            item = list(item)
+            random.shuffle(item)
+            item = ''.join(item)
+            shuffled_words.append(item)
+
+        print(' '.join(shuffled_words))
 
 new_word = Anagram(str(input('Input key:')))
-print(new_word.anagram())
+new_word.anagram()
